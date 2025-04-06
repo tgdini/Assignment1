2c13b582599136c2f019e103656e4b8678e4c056
farishpandai <farishpandai@gmail.com>
2025-04-06 16:24:44 +0800

    Finished_NoComments

--- src/TaskManagerApp.java
+++ src/TaskManagerApp.java
@@ -3,10 +3,15 @@ public class TaskManagerApp {
 
 
     public static void main(String[] args) {
-        Task Task1 = new Task();
-        Task Task = new Task("test","Science",LocalDate.of(2025, 1, 22),"High");
+        Task task1 = new Task();
+        Task taskError = new Task("test","Science",LocalDate.of(2025, 1, 22),"High");
+        Task taskNoError = new Task("test","Science",LocalDate.now().plusDays(5),"High");
 
-        System.out.println(Task.toString());
+        System.out.println(taskError.toString());
+        System.out.println("\n"+TaskValidator.validateTask(taskError)+"\n");
+        System.out.println(taskNoError.toString());
+        System.out.println("\n"+TaskValidator.validateTask(taskNoError));
+        
     }
     
 }
--- src/TaskValidator.java
+++ src/TaskValidator.java
@@ -1,10 +1,44 @@
+import java.time.LocalDate;
+
 public class TaskValidator {
 
 
     public static String validateTask(Task task)
     {
-
         
+        try
+        {
+            if (task.getTaskName().isEmpty()) 
+            {
+                throw new IllegalArgumentException("Task cannot be empty");
+            }
+            else if (task.getCategory().isEmpty()) 
+            {
+                throw new IllegalArgumentException("Category cannot be empty");
+            } 
+            else if (task.getDueDate().isBefore(LocalDate.now())) 
+            {
+                throw new IllegalArgumentException("The Date cannot be before the current date");
+            } 
+            if ((!task.getPriority().equals("High"))&&!task.getPriority().equals("Medium")&&!task.getPriority().equals("Low"))
+            {
+                throw new IllegalArgumentException("Valid Priority values can only be High,Medium or Low");
+            }
+            else {
+
+                return("Valid");
+                
+            }
+            
+        }
+
+        catch(IllegalArgumentException e)
+        {
+            return "Error:" + e.getMessage();
+        }
+
+
+
     }
     
 }
